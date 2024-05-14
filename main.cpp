222d722abdbcc18ba68076da7fe1bef18126e287
s24893-pj <s24893@pjwstk.edu.pl>
2024-05-14 15:27:56 +0200

    Add game background and create game structure

--- main.cpp
+++ main.cpp
@@ -1,41 +1,52 @@
-#include "game.h"
 #include <SDL.h>
 #include <iostream>
-#define SDL_MAIN_HANDLED
 
-int main(int argc, char *argv[])
-{
-	using namespace std;
-	SDL_Init( SDL_INIT_EVERYTHING );
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-	if (auto error = SDL_CreateWindowAndRenderer( 1280, 720,
-		 SDL_WINDOW_SHOWN, &window, &renderer )) {
-		cout << "Nie udalo sie utworzyc okienka" << endl;
-		 }
-	SDL_Event event;
-	int playing = 1;
-	while (playing) {
-		while (SDL_PollEvent(&event)) {
-			switch (event.type)
-			{
-				case SDL_QUIT:
-					playing = false;
-				break;
+int main(int argc, char *argv[]) {
+    using namespace std;
 
-				default:
-					cout << event.type << endl;
-				break;
-			}
-		}
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event event;
 
-		SDL_SetRenderDrawColor(renderer, 255,0,0,255);
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
-	}
-	SDL_DestroyRenderer( renderer );
-	SDL_DestroyWindow( window );
-	//Quit SDL
-	SDL_Quit();
-	return 0;
+    // SDL_Init(SDL_INIT_VIDEO);
+
+    if (auto error = SDL_CreateWindowAndRenderer(1280, 720,
+                                                 SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        cout << "Nie udalo sie utworzyc okienka" << endl;
+    }
+
+    SDL_Surface *surface = SDL_LoadBMP("C:/Users/mateu/CLionProjects/SDL/map_background.bmp");
+    if (surface == nullptr) {
+        cout << "error ;(";
+    }
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+
+    int playing = 1;
+    while (playing) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    playing = false;
+                    break;
+
+                default:
+                    cout << event.type << endl;
+                    break;
+            }
+        }
+
+
+        SDL_SetRenderDrawColor(renderer, 0x00,0x00,0x00,0x00);
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    //Quit SDL
+    SDL_Quit();
+    return 0;
 }
