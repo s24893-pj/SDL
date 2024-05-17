def5727c0d3b60c193681ebd8a99f3083599ba87
s24893-pj <s24893@pjwstk.edu.pl>
2024-05-17 23:46:59 +0200

    Add shooting and shooting collisions

--- game.cpp
+++ game.cpp
@@ -12,7 +12,7 @@ void Game::render(SDL_Renderer *renderer, SDL_Texture *texture) {
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, texture, nullptr, nullptr);
 
-    for (Bullet bullet : bullets) {
+    for (Bullet bullet: bullets) {
         bullet.render(renderer);
     }
 
@@ -20,22 +20,23 @@ void Game::render(SDL_Renderer *renderer, SDL_Texture *texture) {
                      SDL_FLIP_NONE);
     SDL_RenderCopyEx(renderer, player_2.getText(), &player_2.getSource(), &player_2.getDest(), 0, nullptr,
                      SDL_FLIP_NONE);
-
-    SDL_RenderCopyEx(renderer, floor.getText(), &floor.getSource(), &floor.getDest(), 0, nullptr, SDL_FLIP_NONE);
-    SDL_RenderCopyEx(renderer, platformL.getText(), &platformL.getSource(), &platformL.getDest(), 0, nullptr,
-                     SDL_FLIP_NONE);
-    SDL_RenderCopyEx(renderer, platformR.getText(), &platformR.getSource(), &platformR.getDest(), 0, nullptr,
-                     SDL_FLIP_NONE);
+    for (Object wall: walls) {
+        SDL_RenderCopyEx(renderer, wall.getText(), &wall.getSource(), &wall.getDest(), 0, nullptr, SDL_FLIP_NONE);
+        // SDL_RenderCopyEx(renderer, platformL.getText(), &platformL.getSource(), &platformL.getDest(), 0, nullptr,
+        //                  SDL_FLIP_NONE);
+        // SDL_RenderCopyEx(renderer, platformR.getText(), &platformR.getSource(), &platformR.getDest(), 0, nullptr,
+        //                  SDL_FLIP_NONE);
+    }
 
     SDL_RenderPresent(renderer);
 }
 
 void Game::createPlayer(SDL_Renderer *renderer) {
-    player_1.setDest(100, 100, 40, 60);
+    player_1.setDest(60, 500, 40, 60);
     player_1.setImage("C:/Users/mateu/CLionProjects/SDL/img/player1_1.bmp", renderer);
     player_1.setSource(0, 0, 250, 500);
 
-    player_2.setDest(1180, 100, 40, 60);
+    player_2.setDest(1180, 500, 40, 60);
     player_2.setImage("C:/Users/mateu/CLionProjects/SDL/img/player2_1.bmp", renderer);
     player_2.setSource(0, 0, 250, 500);
 }
@@ -44,14 +45,32 @@ void Game::drawMapO(SDL_Renderer *renderer) {
     floor.setDest(0, 695, 1280, 25);
     floor.setImage("C:/Users/mateu/CLionProjects/SDL/img/wall.bmp", renderer);
     floor.setSource(0, 0, 25, 25);
+    walls.push_back(floor);
 
     platformL.setDest(150, 500, 200, 25);
     platformL.setImage("C:/Users/mateu/CLionProjects/SDL/img/wall.bmp", renderer);
     platformL.setSource(0, 0, 25, 25);
+    walls.push_back(platformL);
 
     platformR.setDest(930, 500, 200, 25);
     platformR.setImage("C:/Users/mateu/CLionProjects/SDL/img/wall.bmp", renderer);
     platformR.setSource(0, 0, 25, 25);
+    walls.push_back(platformR);
+
+    platformM.setDest(380, 400, 520, 25);
+    platformM.setImage("C:/Users/mateu/CLionProjects/SDL/img/wall.bmp", renderer);
+    platformM.setSource(0, 0, 25, 25);
+    walls.push_back(platformM);
+
+    WallL.setDest(0, 0, 5, 720);
+    WallL.setImage("C:/Users/mateu/CLionProjects/SDL/img/wall.bmp", renderer);
+    WallL.setSource(0, 0, 25, 25);
+    walls.push_back(WallL);
+
+    WallR.setDest(1275, 0, 5, 720);
+    WallR.setImage("C:/Users/mateu/CLionProjects/SDL/img/wall.bmp", renderer);
+    WallR.setSource(0, 0, 25, 25);
+    walls.push_back(WallR);
 }
 
 
@@ -104,9 +123,12 @@ void Game::updatePlayer(Player &player, int speed) {
         if (l) { newDX -= speed; }
         if (r) { newDX += speed; }
 
-        if (!mapCollision(player, floor, newDX - player.getDX(), 0) &&
-            !mapCollision(player, platformL, newDX - player.getDX(), 0) && !mapCollision(
-                player, platformR, newDX - player.getDX(), 0)) {
+        if (!mapCollision(player, floor, newDX - player.getDX(), 0)
+            && !mapCollision(player, platformL, newDX - player.getDX(), 0)
+            && !mapCollision(player, platformR, newDX - player.getDX(),0)
+            && !mapCollision(player, platformM, newDX - player.getDX(),0)
+            && !mapCollision(player, WallL, newDX - player.getDX(),0)
+            && !mapCollision(player, WallR, newDX - player.getDX(),0)) {
             player.setDest(newDX, player.getDY());
         }
 
@@ -116,9 +138,12 @@ void Game::updatePlayer(Player &player, int speed) {
             player.setFuel(player.getFuel() - 1);
         } else { newDY += speed; }
 
-        if (mapCollision(player, floor, 0, newDY - player.getDY()) ||
-            mapCollision(player, platformL, 0, newDY - player.getDY()) || mapCollision(
-                player, platformR, 0, newDY - player.getDY())) {
+        if (mapCollision(player, floor, 0, newDY - player.getDY())
+            || mapCollision(player, platformL, 0, newDY - player.getDY())
+            || mapCollision(player, platformR, 0, newDY - player.getDY())
+            || mapCollision(player, platformM, 0, newDY - player.getDY())
+            || mapCollision(player, WallL, 0, newDY - player.getDY())
+            || mapCollision(player, WallR, 0, newDY - player.getDY())) {
             fall = false;
             player.setFuel(100);
         }
@@ -135,9 +160,8 @@ void Game::updatePlayer(Player &player, int speed) {
 
         for (Bullet bullet: bullets)
             if (shootCollision(player_1, bullet, 0, 0)) {
-                player_1.setDest(200, 200);
+                player_1.setDest(60, 500);
             }
-
     } else {
         int newDX = player.getDX();
         int newDY = player.getDY();
@@ -145,9 +169,12 @@ void Game::updatePlayer(Player &player, int speed) {
         if (l2) { newDX -= speed; }
         if (r2) { newDX += speed; }
 
-        if (!mapCollision(player, floor, newDX - player.getDX(), 0) &&
-            !mapCollision(player, platformL, newDX - player.getDX(), 0) && !mapCollision(
-                player, platformR, newDX - player.getDX(), 0)) {
+        if (!mapCollision(player, floor, newDX - player.getDX(), 0)
+            && !mapCollision(player, platformL, newDX - player.getDX(), 0)
+            && !mapCollision(player, platformR, newDX - player.getDX(),0)
+            && !mapCollision(player, platformM, newDX - player.getDX(),0)
+            && !mapCollision(player, WallL, newDX - player.getDX(),0)
+            && !mapCollision(player, WallR, newDX - player.getDX(),0)) {
             player.setDest(newDX, player.getDY());
         }
 
@@ -157,9 +184,12 @@ void Game::updatePlayer(Player &player, int speed) {
             player.setFuel(player.getFuel() - 1);
         } else { newDY += speed; }
 
-        if (mapCollision(player, floor, 0, newDY - player.getDY()) ||
-            mapCollision(player, platformL, 0, newDY - player.getDY()) || mapCollision(
-                player, platformR, 0, newDY - player.getDY())) {
+        if (mapCollision(player, floor, 0, newDY - player.getDY())
+            || mapCollision(player, platformL, 0, newDY - player.getDY())
+            || mapCollision(player, platformR, 0, newDY - player.getDY())
+            || mapCollision(player, platformM, 0, newDY - player.getDY())
+            || mapCollision(player, WallL, 0, newDY - player.getDY())
+            || mapCollision(player, WallR, 0, newDY - player.getDY())) {
             fall = false;
             player.setFuel(100);
         }
@@ -174,9 +204,9 @@ void Game::updatePlayer(Player &player, int speed) {
             player.setDest(player.getDX(), player.getDY() + speed);
         }
         for (Bullet bullet: bullets)
-        if (shootCollision(player_2, bullet, 0, 0)) {
-            player_2.setDest(200, 200);
-        }
+            if (shootCollision(player_2, bullet, 0, 0)) {
+                player_2.setDest(1180, 500);
+            }
     }
 }
 
@@ -185,10 +215,9 @@ void Game::update() {
     int speed = 3;
     updatePlayer(player_1, speed);
     updatePlayer(player_2, speed);
-    for(Bullet &bullet : bullets) {
+    for (Bullet &bullet: bullets) {
         bullet.update();
     }
-
 }
 
 bool Game::mapCollision(Player &player, Object &o, int dx, int dy) {
@@ -223,10 +252,10 @@ bool Game::shootCollision(Player &player, Bullet &o, int dx, int dy) {
 
 void Game::shoot(const Player &player) {
     if (player.getId() == 1) {
-        Bullet bullet(player.getDX()+player.getDW(), player.getDY() + player.getDH()/2, 5);
+        Bullet bullet(player.getDX() + player.getDW() + 4, player.getDY() + player.getDH() / 2, 7);
         bullets.push_back(bullet);
     } else {
-        Bullet bullet(player.getDX()-10, player.getDY() + player.getDH()/2, -5);
+        Bullet bullet(player.getDX() - 10, player.getDY() + player.getDH() / 2, -7);
         bullets.push_back(bullet);
     }
 }
